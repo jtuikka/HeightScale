7c7285d175c37ecf4bcd46bdfec6359a7ae36c57
jtuikka <jtuikka22@student.oulu.fi>
2025-10-15 16:43:12 +0300

    Basic height calculation

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Mi Scale Automation...
+echo Note: Make sure your Xiaomi Mi Scale is powered on and nearby
+echo Press Ctrl+C to stop the script
+echo.
+C:\Users\Janne\mi-scale-automation\.venv\Scripts\python.exe C:\Users\Janne\HeightScale\HeightScale\scan.py
+pause
---
+++ scan.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+
+from bleak import BleakClient, BleakScanner
+from bleak.backends.characteristic import BleakGATTCharacteristic
+
+from parsing import parse_body_composition_message
+
+BODY_COMPOSITION_MEASUREMENT_UUID = "00002a9c-0000-1000-8000-00805f9b34fb"
+
+logger = logging.getLogger(__name__)
+
+async def find_miscale_device():
+    return await BleakScanner().find_device_by_name("MIBFS")
+
+
+def notification_handler(_characteristic: BleakGATTCharacteristic, data: bytearray):
+    """parses body composition data and logs it"""
+    message = parse_body_composition_message(data)
+
+    logger.debug(message)
+
+    if message.stabilized and message.measurement.impedance < 3000:
+        # when the measurement is stable and the impedance is below 3000 ohm, the measurement is valid
+        logger.info(message)
+
+
+async def connect_and_measure():
+    disconnected_event = asyncio.Event()
+
+    def disconnected_callback(_bleak_client: BleakClient):
+        logger.info("disconnected callback")
+        disconnected_event.set()
+
+    device = await find_miscale_device()
+    logger.info("found device: %s", device.name if device else "None")
+    if not device:
+        logger.info("no device found")
+        return
+
+    client = BleakClient(device, disconnected_callback=disconnected_callback)
+
+    async with client:
+        await client.start_notify(
+            BODY_COMPOSITION_MEASUREMENT_UUID, notification_handler
+        )
+        await disconnected_event.wait()
+
+
+async def main():
+    logger.info("starting scan")
+    while True:
+        await connect_and_measure()
+        logger.info("restarting scan")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
+    )
+
+    asyncio.run(main())
